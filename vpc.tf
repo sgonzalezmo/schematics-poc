6ccbb69453277d7a6345547f103b40e9b7dfc871
Samuel Gonzalez Morro <sgonzalezmo@gmail.com>
2020-12-17 11:20:16 +0100

    Add tags to VPC

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,5 @@
 resource "ibm_is_vpc" "vpc1" {
   name                      = "${lower(var.system)}-${lower(var.environment)}-vpc"
   address_prefix_management = "auto"
+  tags                      = [var.system, var.environment]
 }
