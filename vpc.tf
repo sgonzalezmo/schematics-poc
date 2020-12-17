2c2594d633f21febbcfb5cdc4f1834ea781c2342
Samuel Gonzalez Morro <sgonzalezmo@gmail.com>
2020-12-17 11:12:54 +0100

    System and environment name to lower.

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,4 @@
 resource "ibm_is_vpc" "vpc1" {
-  name                      = "${var.system}-${var.environment}-vpc"
+  name                      = "${lower(var.system)}-${lower(var.environment)}-vpc"
   address_prefix_management = "auto"
 }
