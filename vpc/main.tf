abad7da45d218244a075d83730ddfed6001dec73
Samuel Gonzalez Morro <sgonzalezmo@gmail.com>
2020-12-17 16:10:43 +0100

    Add vpc subnets creation.

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,14 +1,29 @@
 locals {
   system_lower      = lower(var.system)
   environment_lower = lower(var.environment)
+  vpc_name          = "${local.system_lower}-${local.environment_lower}-vpc"
+  vpc_tags          = ["system:${local.system_lower}", "environment:${local.environment_lower}"]
 }
 
-resource "ibm_is_vpc" "vpc1" {
-  name                      = "${local.system_lower}-${local.environment_lower}-vpc"
+resource "ibm_is_vpc" "vpc" {
+  name                      = local.vpc_name
   address_prefix_management = "auto"
-  tags                      = ["system:${local.system_lower}", "environment:${local.environment_lower}"]
+  tags                      = local.vpc_tags
 }
 
-data "ibm_is_vpc" "ds_vpc" {
-  name = "${local.system_lower}-${local.environment_lower}-vpc"
+resource "ibm_is_vpc_address_prefix" "vpc-ap" {
+  for_each = var.subnets_cidr
+  name     = "vpc-ap-${each.key}"
+  zone     = each.key
+  vpc      = ibm_is_vpc.vpc.id
+  cidr     = each.value
+}
+
+resource "ibm_is_subnet" "vpc_subnet" {
+  for_each        = var.subnets_cidr
+  name            = "vpc-subnet-${each.key}"
+  vpc             = ibm_is_vpc.vpc.id
+  zone            = each.key
+  ipv4_cidr_block = each.value
+  depends_on      = [ibm_is_vpc_address_prefix.vpc-ap]
 }
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -7,3 +7,8 @@ variable "environment" {
   type        = string
   description = ""
 }
+
+variable "subnets_cidr" {
+  type        = map(string)
+  description = ""
+}
